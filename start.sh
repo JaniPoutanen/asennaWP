6948092354ae723222939d3986841130f7914fcb
Jani Poutanen <janipoutanen@gmail.com>
2017-05-16 16:14:36 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,9 @@
 
 sudo apt-get update
 sudo apt-get -y install puppet git
-git config --global user.email "janipoutanen@gmail.com"
-git config --global user.name "jani"
-git config --global credential.helper "cache --timeout=7200"
+git config --global credential.helper "cache --timeout=3600"
 git clone https://github.com/JaniPoutanen/asennaWP.git
 cd asennaWP
+sudo puppet apply --modulepath modules/ -e 'class  {"asennawp":}'
 sudo cp -r modules /etc/puppet/
 bash apply.sh
